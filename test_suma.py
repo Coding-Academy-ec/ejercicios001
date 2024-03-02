b731b3aa73bc855cb62c4827cd4e57c15732d619
Diego Saavedra <dmsaavedra@espe.edu.ec>
2024-03-01 19:49:00 -0500

    first commit

---
+++ test_suma.py
@@ -0,0 +1,45 @@
+# 001 --- suma
+
+from codigo import sumar
+
+def test_suma():
+    assert sumar(2, 3) == 5
+    assert sumar(-1, 1) == 0
+    assert sumar(0, 0) == 0
+
+# --- Factorial
+    
+from codigo import sumar
+
+def test_suma():
+    assert sumar(2, 3) == 5
+    assert sumar(-1, 1) == 0
+    assert sumar(0, 0) == 0
+
+# 003 --- 
+    
+def contar_vocales(cadena):
+  vocales = 'aeiouAEIOU'
+  contador = 0
+  for letra in cadena:
+      if letra in vocales:
+          contador += 1
+  return contador
+
+# 004 ---
+
+from codigo import es_palindromo
+
+def test_es_palindromo():
+    assert es_palindromo("radar") == True
+    assert es_palindromo("oso") == True
+    assert es_palindromo("Python") == False
+
+# 005 ---
+    
+from codigo import suma_lista
+
+def test_suma_lista():
+    assert suma_lista([1, 2, 3, 4, 5]) == 15
+    assert suma_lista([-1, -2, -3, -4, -5]) == -15
+    assert suma_lista([0, 0, 0, 0, 0]) == 0
